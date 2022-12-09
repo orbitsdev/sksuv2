9a9a898de146d540076893a9dfd66c1ad2928a63
brian <orbinobrian0506@gmail.com>
2022-12-09 21:18:26 +0800

    modifed

--- app/Http/Controllers/Api/FileController.php
+++ app/Http/Controllers/Api/FileController.php
@@ -12,63 +12,65 @@ class FileController extends Controller
 {
 
 
-        static function storeFiles($files ,$owner, $storageDisk, $model){
+    static function storeFiles($files, $owner, $disk, $model)
+    {
 
-            foreach ($files  as $file) {
+        foreach ($files  as $file) {
 
-                $createdfile = File::create([
-                    'owned_by' => $owner,
-                    'folder' => $file["folder"],
-                    'file_name' => $file["file_name"],
-                    'file_type' => $file["file_type"],
-                ]);
+            $createdfile = File::create([
+                'owned_by' => $owner,
+                'folder' => $file["folder"],
+                'file_name' => $file["file_name"],
+                'file_type' => $file["file_type"],
+            ]);
 
-                $fromTemporaryFolder = 'tmp/' . $createdfile['folder'] . '/' . $createdfile['file_name'];
-                $toActualFilesFolder = 'files/' . $createdfile['owned_by'] . '/' . $createdfile['folder'] . '/' . $createdfile['file_name'];
-                $temporaryFolderOfFile = 'tmp/' . $createdfile['folder'];
+            $fromTemporaryFolder = 'tmp/' . $createdfile['folder'] . '/' . $createdfile['file_name'];
+            $toActualFilesFolder = 'files/' . $createdfile['owned_by'] . '/' . $createdfile['folder'] . '/' . $createdfile['file_name'];
+            $temporaryFolderOfFile = 'tmp/' . $createdfile['folder'];
 
-                Storage::disk($storageDisk)->copy($fromTemporaryFolder, $toActualFilesFolder);
-                
-                $url  = Storage::disk($storageDisk)->url($toActualFilesFolder);                
-                $createdfile->update([
-                    'url'=> $url
-                ]);
+            Storage::disk($disk)->copy($fromTemporaryFolder, $toActualFilesFolder);
 
-                Storage::disk($storageDisk)->deleteDirectory($temporaryFolderOfFile);
-                TemporaryStorage::where('folder', $createdfile['folder'])->delete();
-                $model->files()->attach($createdfile->id);
-            }
+            $url  = Storage::disk($disk)->url($toActualFilesFolder);
+            $createdfile->update([
+                'url' => $url
+            ]);
 
+            Storage::disk($disk)->deleteDirectory($temporaryFolderOfFile);
+            TemporaryStorage::where('folder', $createdfile['folder'])->delete();
+            $model->files()->attach($createdfile->id);
+        }
+    }
+
+    static function removeFiles($filestoremove, $disk, $model)
+    {
+
+        $filecollection = [];
+
+        foreach ($filestoremove as $file) {
+            $filecollection[] = $file['id'];
         }
 
-        static function removeFiles($filestoremove, $disk, $model){
-           
-            $filecollection = [];
+        $databasefile  = File::whereIn('id', $filecollection)->get();
 
-            foreach ($filestoremove as $file) {
-                $filecollection[] = $file['id'];
-            }
-
-            $databasefile  = File::whereIn('id', $filecollection)->get();
-            
-            foreach ($databasefile as $file) {
-                $filefolder  = 'files/'.$file['owned_by'].'/'. $file['folder'];
-                Storage::disk($disk)->deleteDirectory($filefolder);
-                $model->files()->detach($file['id']);
-            }
-            File::whereIn('id', $filecollection)->delete();
+        foreach ($databasefile as $file) {
+            $filefolder  = 'files/' . $file['owned_by'] . '/' . $file['folder'];
+            Storage::disk($disk)->deleteDirectory($filefolder);
+            $model->files()->detach($file['id']);
         }
-        
-        static function deleteAttachedFiles($model, $disk){
-            
-            foreach ($model->files as $file) {
+        File::whereIn('id', $filecollection)->delete();
+    }
 
-                File::where('folder', $file['folder'])->delete();
-                $filesFolder = 'files/' . $file['owned_by'] . '/' . $file['folder'];
-                Storage::disk($disk)->deleteDirectory($filesFolder);
-            }
+    static function deleteAttachedFiles($model, $disk)
+    {
 
+        foreach ($model->files as $file) {
 
-            $model->files()->detach();
+            File::where('folder', $file['folder'])->delete();
+            $filesFolder = 'files/' . $file['owned_by'] . '/' . $file['folder'];
+            Storage::disk($disk)->deleteDirectory($filesFolder);
         }
+
+
+        $model->files()->detach();
+    }
 }
