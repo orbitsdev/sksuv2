02002a24bdabc0640b7f88328931500e39ea7330
Brian Orbino <86046953+orbitsdev@users.noreply.github.com>
2023-01-18 12:50:53 +0800

    Add Edit Form

--- resources/js/views/SBO-STUDENT/ApplicationPage.vue
+++ resources/js/views/SBO-STUDENT/ApplicationPage.vue
@@ -9,12 +9,21 @@
     <div v-else>
       <div v-if="response != null">
         <div class="overflow-hidden bg-white shadow sm:rounded-lg">
+          <div class="flex items-center justify-between">
           <div class="px-4 py-5 sm:px-6">
             <h3 class="text-lg font-extrabold leading-6">
               {{ response.application_form.title }}
             </h3>
             <p class="mt-1 max-w-2xl text-sm font-bold">General Information</p>
           </div>
+          <div class="px-8">
+              <button @click="showForm = true" class="border px-2 py-1 hover:shadow-lg cursor-pointer rounded-lg text-white bg-green-700 ">
+                <div class="flex items-center ">
+                  <span class="text-sm mr-1">Edit</span>  <i class="text-xs fa-solid fa-pencil"> </i>
+                </div>
+              </button>
+          </div>
+        </div>
           <div class="border-t border-gray-200 px-4 py-5 sm:p-0">
             <dl class="sm:divide-y sm:divide-gray-200">
               <div
@@ -41,34 +50,40 @@
               <div class="py-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:py-5 sm:px-6" v-else>
                 <!-- {{ response.response_requirements }} -->
              
-                <dt class="text-sm font-medium text-gray-500">Attachments</dt>
+                <p class="mt-1 max-w-2xl text-sm font-bold">Requirement Attachment</p>
                 <dd class="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0">
                    
                    <!-- {{ response.response_requirements}} -->
                     <div v-for="rf in  response.response_requirements" :key="rf.id">
-                        <p class="capitalize font-semibold mb-1"> {{ rf.requirement.name }}</p>
+                        <p class="capitalize font-semibold mb-2"> {{ rf.requirement.name }}</p>
 
-                        <div v-if="rf.files.length > 0" class=" my-1 ">
+                        <div v-if="rf.files.length > 0" class="  ">
                           
-                            <div  class="" v-for=" file in rf.files " :key="file.id">
-                                <!-- Heroicon name: mini/paper-clip -->
-                                <div>
-                                    <div class="inline-flex flex-nowrap items-center justify-center border  px-2 rounded py-1">                                    
-                                        <svg class="h-5 w-5 mr-1  text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                            <div  class="inline-flex  mr-2 mb-1 text-green-800 bg-green-100  font-bold text-sm  flex-nowrap items-center justify-center   px-2 rounded-lg py-1 hover:shadow-lg cursor-pointer " v-for=" file in rf.files " :key="file.id">
+                                <div class="mb-0.5">
+                                    <div class="flex flex-wrap items-center">                                    
+                                        <svg class="h-5 w-5 mr-1  text-green-800 font-bold " xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                             <path fill-rule="evenodd" d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z" clip-rule="evenodd" />
                                         </svg>
                                         <span class="">{{file.file_name}}</span>
-                                        <div class="ml-2 text-xs uppercase"> <i class=" fa-regular fa-file"></i></div>
+                                        <!-- <div class="ml-2 text-xs uppercase"> <i class=" fa-regular fa-file"></i></div> -->
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div v-else>
-                            None 
+                        <div v-else class="flex items-center  rounded-full  ">
+                          <div  class="inline-flex  mr-2 mb-1 text-gray-400    text-sm  flex-nowrap items-center justify-center   px-2 py-1  " >
+                            <div class="mb-0.5">
+                                <div class="flex flex-wrap items-center">                                    
+                                    
+                                    <span class="">No file</span>
+                                    <!-- <div class="ml-2 text-xs uppercase"> <i class=" fa-regular fa-file"></i></div> -->
+                                </div>
+                            </div>
                         </div>
-                        
+      
+                        </div>
                     </div>
-
 
                 </dd>
               </div>
@@ -83,24 +98,72 @@
         />
       </div>
     </div>
+
+    <teleport to="#app">
+      <BaseDialog :show="showForm" :width="'600'" :preventClose="true">
+        <template #c-content>
+          <EditApplication :data="response"/>
+
+          <div class="my-4"></div>
+          <div class="my-2 flex justify-end">
+            <TableButton mode class="mr-2" @click="showForm = false"> Close </TableButton>
+            <div class="my-1 mx-2" v-if="isSaving">
+              <BaseSpinner />
+            </div>
+            <div v-else>
+              <TableButton
+             
+                class=""
+                @click="updateResponse"
+              >
+                Save
+              </TableButton>
+              
+            </div>
+          </div>
+        </template>
+
+       
+      </BaseDialog>
+    </teleport>
   </div>
 </template>
 
 <script>
+import EditApplication from './EditApplication.vue';
 import axiosApi from "../../api/axiosApi";
 export default {
   props: ["id"],
 
+  components: {
+    EditApplication,
+  },
   data() {
     return {
       isFetching: false,
       response: null,
+      showForm: false,
+      isSaving: false,
     };
   },
   created() {
     this.getResponse();
   },
   methods: {
+
+
+    updateResponse(){
+                console.log("yow");
+            },
+
+            
+    showEditForm(){
+
+      this.showEditForm = true;
+
+
+
+    },
     async getResponse() {
       this.isFetching = true;
 
---
+++ resources/js/views/SBO-STUDENT/EditApplication.vue
@@ -0,0 +1,35 @@
+
+
+<template>
+    <div>
+        <h3 class="text-lg font-extrabold leading-6" v-if="application != null">
+            {{ application }}
+            
+            Application Title
+          </h3>
+          <p class="mt-1 max-w-2xl text-sm font-bold">General Information</p>
+
+          
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['data'],
+ 
+        data() {
+            return {
+                application: null,
+                isSaving: false,
+            }
+        },
+
+        methods: {
+            
+        }
+    }
+</script>
+
+<style  scoped>
+
+</style>
