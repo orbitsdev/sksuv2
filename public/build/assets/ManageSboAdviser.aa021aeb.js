5143d8aced9f2d58021be0270d015a0d91160dd5
Benits <66729989+projectorb@users.noreply.github.com>
2023-02-18 22:09:02 +0800

    build v8

---
+++ public/build/assets/ManageSboAdviser.aa021aeb.js
@@ -0,0 +1,1 @@
+import{_ as o,c as s,d as t,o as a,r}from"./app.0b9d15e2.js";const n={created(){this.$router.push({name:"manage-sbo-adviser-user"})}};function _(c,d,p,i,u,l){const e=r("router-view");return a(),s("main",null,[t(e)])}const v=o(n,[["render",_],["__scopeId","data-v-14006492"]]);export{v as default};
--- resources/js/api/axiosApi.js
+++ resources/js/api/axiosApi.js
@@ -7,7 +7,7 @@ let axiosApi =  axios.create({});
 const token = localStorage.getItem('token');
 
 
-axiosApi.defaults.baseURL = "http://127.0.0.1:8000/";
+axiosApi.defaults.baseURL = "https://wrms-accreditation.projectorb.shop/";
 axiosApi.defaults.headers.common['Content-Type'] = "application/json";
 axiosApi.defaults.headers.common['Accept'] = "application/json";
 axiosApi.defaults.headers.common['Authorization'] = "Bearer " + token;
