578dc0c76acc63be8c2bf1d3aeb95278f92e0963
Benits <66729989+projectorb@users.noreply.github.com>
2023-02-18 21:45:10 +0800

    build v5

--- public/build/assets/SocialAccountTokenCatcher.24a59f86.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,g as t,o as n,r}from"./app.1c991082.js";const c={created(){this.loginUser()},methods:{loginUser(){localStorage.setItem("token",this.$route.query.code),localStorage.getItem("token")&&window.location.reload(!0)}}};function a(s,i,l,d,_,p){const e=r("LoadingScreen");return n(),t(e)}const m=o(c,[["render",a]]);export{m as default};
