f76e8b57cb0ab896e07c5de0dd4f29a95386068b
Benits <66729989+projectorb@users.noreply.github.com>
2023-02-18 22:35:54 +0800

    vite access env variable to api

--- resources/js/api/axiosApi.js
+++ resources/js/api/axiosApi.js
@@ -6,8 +6,7 @@ import axios from "axios"
 let axiosApi =  axios.create({});
 const token = localStorage.getItem('token');
 
-
-axiosApi.defaults.baseURL = "http://127.0.0.1:8000/";
+axiosApi.defaults.baseURL = import.meta.env.VITE_PUSHER_APP_URL;
 axiosApi.defaults.headers.common['Content-Type'] = "application/json";
 axiosApi.defaults.headers.common['Accept'] = "application/json";
 axiosApi.defaults.headers.common['Authorization'] = "Bearer " + token;
