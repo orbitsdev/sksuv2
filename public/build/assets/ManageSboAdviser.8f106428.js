9d058baca5a8ccdf46e7ef5b9e3485610fc74bcf
Benits <66729989+projectorb@users.noreply.github.com>
2023-02-18 20:10:21 +0800

    build v2

--- public/build/assets/ManageSboAdviser.8f106428.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as s,d as t,o as a,r}from"./app.a30414ce.js";const n={created(){this.$router.push({name:"manage-sbo-adviser-user"})}};function _(c,d,p,i,u,l){const e=r("router-view");return a(),s("main",null,[t(e)])}const v=o(n,[["render",_],["__scopeId","data-v-14006492"]]);export{v as default};
--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,14 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
  
 export default defineConfig({
+  server: {
+    host: 'wrms-accreditation.projectorb.shop',
+    https : true,
+  },
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
+                'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
