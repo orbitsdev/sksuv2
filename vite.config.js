e4ab1d76036783d758307682d0c7f6000fbdda80
Benits <66729989+projectorb@users.noreply.github.com>
2023-02-18 20:17:31 +0800

    build v4

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,17 @@
-
-
-
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
- 
 export default defineConfig({
   server: {
     host: 'wrms-accreditation.projectorb.shop',
     https : true,
   },
     plugins: [
+       
         laravel({
             input: [
-              'resources/css/app.css',
-              'resources/js/app.js',
+                'resources/css/app.css',
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
@@ -27,6 +24,7 @@ export default defineConfig({
             },
         }),
     ],
+   
     resolve: {
         alias: {
             vue: 'vue/dist/vue.esm-bundler.js',
